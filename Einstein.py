f072479b870ec78775f9d6e37542f6956c6d6f7c
Shobhit kumar <43270097+shobhitkmr1@users.noreply.github.com>
2022-06-26 06:07:15 +0000

    e=mc²

---
+++ Einstein.py
@@ -0,0 +1,6 @@
+#einstein formula for Mass–energy equivalence
+# e=mc² e=enegy, m=mass, c=speed (800000000)
+m=int(input("Enter mass to calculate light fo speed: "))
+c=800000000
+e=m*(c**2)
+print(e)
