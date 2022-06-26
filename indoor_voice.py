1560233e91d4457aed300d3fd8d70a219cf5b367
Shobhit kumar <43270097+shobhitkmr1@users.noreply.github.com>
2022-06-26 03:04:48 +0000

    Indoor_Voice

---
+++ indoor_voice.py
@@ -0,0 +1,6 @@
+#getting input from users
+Input=input("Enter some input: ")
+#convert all string into lowercase
+print("Convert into Lowercase: ",Input.lower())
+#convert all string into Uppercase
+print("Convert into Uppercase: ",Input.upper())
