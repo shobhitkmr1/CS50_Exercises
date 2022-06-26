3ff5b6bc1b73f68dbd000135e4c7705605e64386
Shobhit kumar <43270097+shobhitkmr1@users.noreply.github.com>
2022-06-26 03:13:26 +0000

    playback_speed

---
+++ playback_speed.py
@@ -0,0 +1,8 @@
+#getting input string from users
+Input=input("Enter text: ")
+#change whitescape with 3 dots 
+print(Input.replace(" ", "..."))\
+#change whitescape with underscore
+print(Input.replace(" ","_"))
+#change underscore with whitespace
+print(Input.replace("_"," "))
